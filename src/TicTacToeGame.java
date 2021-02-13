2b2cce8ae4676ef030d902bb38bbf90e24a718dd
shravan-kaja <shravan.kaja@gmail.com>
2021-02-13 11:38:53 +0530

    Added: functionality to take input form useer and adding it to board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -7,7 +7,7 @@ public class TicTacToeGame {
 	static char[] empty_Board = new char[10];
 
 	public static char[] uc1_Initialize() {
-		
+
 		for (int i = 0; i < 10; i++) {
 			empty_Board[i] = ' ';
 		}
@@ -30,24 +30,29 @@ public class TicTacToeGame {
 		}
 		return input_User;
 	}
+
 	public static void uc3_Display_Board() {
-		 
-	        System.out.println("|---|---|---|"); 
-	        System.out.println("| " + empty_Board[1] + " | "
-	                           + empty_Board[2] + " | " + empty_Board[3] 
-	                           + " |"); 
-	        System.out.println("|-----------|"); 
-	        System.out.println("| " + empty_Board[4] + " | "
-	                           + empty_Board[5] + " | " + empty_Board[6] 
-	                           + " |"); 
-	        System.out.println("|-----------|"); 
-	        System.out.println("| " + empty_Board[7] + " | "
-	                           + empty_Board[8] + " | " + empty_Board[9] 
-	                           + " |"); 
-	        System.out.println("|---|---|---|"); 
-	     
+
+		System.out.println("|---|---|---|");
+		System.out.println("| " + empty_Board[1] + " | " + empty_Board[2] + " | " + empty_Board[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + empty_Board[4] + " | " + empty_Board[5] + " | " + empty_Board[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + empty_Board[7] + " | " + empty_Board[8] + " | " + empty_Board[9] + " |");
+		System.out.println("|---|---|---|");
+
 	}
 
+	public static void uc4_Take_Input() {
+		System.out.println("Enter the cell you want to enter");
+		Scanner input = new Scanner(System.in);
+		int input_Position = input.nextInt();
+		if (empty_Board[input_Position] == ' ') {
+			empty_Board[input_Position] = input_User1;
+		} else {
+			System.out.println("Position filled");
+		}
+	}
 
 	public static void main(String Args[]) {
 		System.out.println("Welcome to Tic Tac Toe");
@@ -56,6 +61,10 @@ public class TicTacToeGame {
 		System.out.println("User Selected input :" + input_User1);
 		System.out.println("Comp selected input :" + comp_Input);
 		uc3_Display_Board();
-	}
+		uc4_Take_Input();
+		uc3_Display_Board();
+		uc4_Take_Input();
+		uc3_Display_Board();
 
+	}
 }
