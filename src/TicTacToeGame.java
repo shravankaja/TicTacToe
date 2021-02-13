14f1e2665d8721c24f84df8aededee6ee59be3c2
shravan-kaja <shravan.kaja@gmail.com>
2021-02-13 09:50:22 +0530

    Refactored: added empty space in array of char

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -9,21 +9,15 @@ public class TicTacToeGame {
 	public static char p_20;
 	public static char p_21;
 	public static char p_22;
+
 	public static void uc1_Initialize() {
-		char[] empty_Board=new char[10];
-		
-		 empty_Board[1]=p_00;
-		 empty_Board[2]=p_01;
-		 empty_Board[3]=p_02;
-		 empty_Board[4]=p_10;
-		 empty_Board[5]=p_11;
-		 empty_Board[6]=p_12;
-		 empty_Board[7]=p_20;
-		 empty_Board[8]=p_21;
-		 empty_Board[9]=p_22;
+		char[] empty_Board = new char[10];
+		for (int i = 1; i <= 10; i++) {
+			empty_Board[i] = ' ';
+		}
 	}
-	public static void main(String Args[])
-	{
+
+	public static void main(String Args[]) {
 		System.out.println("Welcome to Tic Tac Toe");
 		uc1_Initialize();
 	}
