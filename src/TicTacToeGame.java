34532ffecad5497dbf760be8d5fa24d7c623d9e8
shravan-kaja <shravan.kaja@gmail.com>
2021-02-15 17:10:18 +0530

    Added: method to check for winner

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -7,6 +7,9 @@ public class TicTacToeGame {
 	static char[] empty_Board = new char[10];
 	public static float turn;
 	public static boolean check;
+	public static int input_count;
+	static int rand_Input_Comp;
+	static int chance;
 
 	public static char[] uc1_Initialize() {
 
@@ -45,7 +48,7 @@ public class TicTacToeGame {
 
 	}
 
-	public static boolean uc6_First_Chance() {
+	public static void uc6_First_Chance() {
 		turn = Math.round(Math.random());
 		if (turn == 0.0) {
 			System.out.println("User wins first Chance");
@@ -54,22 +57,116 @@ public class TicTacToeGame {
 			System.out.println("Comp wins turn");
 			check = false;
 		}
-		return true;
+
 	}
 
 	public static void uc4_Take_Input() {
-		System.out.println("Enter the cell you want to enter");
-		Scanner input = new Scanner(System.in);
-		int input_Position = input.nextInt();
-		if (input_Position > 0 && input_Position < 10) {
-			if (empty_Board[input_Position] == ' ') {
-				empty_Board[input_Position] = input_User1;
-			} else {
-				System.out.println("Position filled");
+		while (true) {
+			if (input_count == 9) {
+				System.out.println("No more turn left");
+				System.exit(0);
+				break;
 			}
-		} else {
-			System.out.println("please enter valid cell range");
+			System.out.println("Enter the cell you want to enter");
+			Scanner input = new Scanner(System.in);
+			int input_Position = input.nextInt();
+			if (input_Position > 0 && input_Position < 10) {
+
+				if (empty_Board[input_Position] == ' ') {
+					empty_Board[input_Position] = input_User1;
+					input_count++;
+					break;
+				} else {
+					System.out.println("Position filled");
+					continue;
+				}
+
+			} else {
+				System.out.println("please enter valid cell range");
+				continue;
+			}
 		}
+	}
+
+	public static void uc7_Win_Tie_Change() {
+		if (empty_Board[1] == 'X' && empty_Board[2] == 'X' && empty_Board[3] == 'X' && check == true
+				|| empty_Board[1] == 'O' && empty_Board[2] == 'O' && empty_Board[3] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);
+		} else if (empty_Board[1] == 'X' && empty_Board[2] == 'X' && empty_Board[3] == 'X' && check == false
+				|| empty_Board[1] == 'O' && empty_Board[2] == 'O' && empty_Board[3] == 'O' && check == false) {
+			System.out.println("Comp wins \n");
+			System.exit(0);
+		} else if (empty_Board[4] == 'X' && empty_Board[5] == 'X' && empty_Board[6] == 'X' && check == false
+				|| empty_Board[4] == 'O' && empty_Board[5] == 'O' && empty_Board[6] == 'O' && check == false) {
+			System.out.println("Comp Wins");
+			System.exit(0);
+		} else if (empty_Board[7] == 'X' && empty_Board[8] == 'X' && empty_Board[9] == 'X' && check == false
+				|| empty_Board[7] == 'O' && empty_Board[8] == 'O' && empty_Board[9] == 'O' && check == false) {
+			System.out.println("Comp Wins");
+			System.exit(0);
+		} else if (empty_Board[4] == 'X' && empty_Board[5] == 'X' && empty_Board[6] == 'X' && check == true
+				|| empty_Board[4] == 'O' && empty_Board[5] == 'O' && empty_Board[6] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);System.exit(0);
+		} else if (empty_Board[7] == 'X' && empty_Board[8] == 'X' && empty_Board[9] == 'X' && check == true
+				|| empty_Board[7] == 'O' && empty_Board[8] == 'O' && empty_Board[9] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);
+		} else if (empty_Board[1] == 'X' && empty_Board[4] == 'X' && empty_Board[7] == 'X' && check == true
+				|| empty_Board[1] == 'O' && empty_Board[4] == 'O' && empty_Board[7] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);
+		} else if (empty_Board[2] == 'X' && empty_Board[5] == 'X' && empty_Board[8] == 'X' && check == true
+				|| empty_Board[2] == 'O' && empty_Board[5] == 'O' && empty_Board[8] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);
+		} else if (empty_Board[3] == 'X' && empty_Board[6] == 'X' && empty_Board[9] == 'X' && check == true
+				|| empty_Board[3] == 'O' && empty_Board[6] == 'O' && empty_Board[9] == 'O' && check == true) {
+			System.out.println("User wins \n");
+			System.exit(0);
+		} else if (empty_Board[1] == 'X' && empty_Board[4] == 'X' && empty_Board[7] == 'X' && check == false
+				|| empty_Board[1] == 'O' && empty_Board[4] == 'O' && empty_Board[7] == 'O' && check == false) {
+			System.out.println("Comp wins \n");
+			System.exit(0);
+		} else if (empty_Board[2] == 'X' && empty_Board[5] == 'X' && empty_Board[8] == 'X' && check == false
+				|| empty_Board[2] == 'O' && empty_Board[5] == 'O' && empty_Board[8] == 'O' && check == false) {
+			System.out.println("Comp wins \n");
+			System.exit(0);
+		} else if (empty_Board[3] == 'X' && empty_Board[6] == 'X' && empty_Board[9] == 'X' && check == false
+				|| empty_Board[3] == 'O' && empty_Board[6] == 'O' && empty_Board[9] == 'O' && check == false) {
+			System.out.println("Comp wins \n");
+			System.exit(0);
+		} else {
+			System.out.println("No winner yet");
+		}
+	}
+
+	public static void comp_Input() {
+		while (true) {
+			if (input_count == 9) {
+				System.out.println("No more turn left");
+				System.exit(0);
+				break;
+			}
+			Random rn = new Random();
+			rand_Input_Comp = rn.nextInt(10);
+
+			if (rand_Input_Comp > 0 && rand_Input_Comp < 10) {
+				if (empty_Board[rand_Input_Comp] == ' ') {
+					empty_Board[rand_Input_Comp] = comp_Input;
+					input_count++;
+					break;
+				} else {
+					System.out.println("");
+					continue;
+				}
+			} else {
+				System.out.println("");
+				continue;
+			}
+		}
+
 	}
 
 	public static void main(String Args[]) {
@@ -79,20 +176,37 @@ public class TicTacToeGame {
 		System.out.println("User Selected input :" + input_User1);
 		System.out.println("Comp selected input :" + comp_Input);
 		uc3_Display_Board();
-		check = uc6_First_Chance();
-		System.out.println(check);
+		uc6_First_Chance();
+		if (check == true) {
+			uc4_Take_Input();
+			chance = 0;
+			uc7_Win_Tie_Change();
+			uc3_Display_Board();
+			System.out.println("----------------------------------------------------------------");
+		} else if (check == true) {
+			comp_Input();
+			chance = 1;
+			uc7_Win_Tie_Change();
+			uc3_Display_Board();
+			System.out.println("----------------------------------------------------------------");
+		}
 		while (true) {
-
-			if (check == true) {
-				uc4_Take_Input();
+			if (chance == 0) {
+				comp_Input();
+				chance = 1;
+				uc7_Win_Tie_Change();
 				uc3_Display_Board();
+				System.out.println("----------------------------------------------------------------");
+				continue;
+			} else if (chance == 1) {
 				uc4_Take_Input();
+				chance = 0;
+				uc7_Win_Tie_Change();
 				uc3_Display_Board();
-				break;
-			} else {
-				System.out.println("Computer turn");
-				break;
+				System.out.println("----------------------------------------------------------------");
+				continue;
 			}
 		}
+
 	}
 }
